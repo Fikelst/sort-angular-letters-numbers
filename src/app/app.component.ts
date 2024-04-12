89e2c4d0d85b34fa6a0ba13964b18aa545ea2a2e
fikelst <fikelst@gmail.com>
2024-04-12 11:32:05 +0200

    Your commit message here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projekt';
+  title = 'angular-app';
+  inputText: string = '';
+  sortedItems: any = '';
+  sortAscending: boolean = true;
+
+  sortItems() {
+    // Jeśli wprowadzony tekst zawiera tylko cyfry, sortujemy liczby
+    if (/^\d+$/.test(this.inputText)) {
+      // Jeśli sortujemy od A do Z
+      if (this.sortAscending) {
+        this.sortedItems = this.inputText.split('').map(Number).sort((a, b) => a - b);
+      } else { // Jeśli sortujemy od Z do A
+        this.sortedItems = this.inputText.split('').map(Number).sort((a, b) => b - a);
+      }
+    } else { // Jeśli wprowadzony tekst zawiera litery, sortujemy litery
+      // Jeśli sortujemy od A do Z
+      if (this.sortAscending) {
+        this.sortedItems = this.inputText.split('').sort().join('');
+      } else { // Jeśli sortujemy od Z do A
+        this.sortedItems = this.inputText.split('').sort().reverse().join('');
+      }
+    }
+    // Zmiana kolejności sortowania na przeciwną
+    this.sortAscending = !this.sortAscending;
+  }
 }
